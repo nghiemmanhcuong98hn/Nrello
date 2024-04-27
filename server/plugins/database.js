92bc02c7de1a29b1eece7ceee920e62a4d9ddf2a
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2024-04-27 07:44:07 +0700

    fix url mongose

--- server/plugins/database.js
+++ server/plugins/database.js
@@ -4,10 +4,7 @@ export default defineNitroPlugin(async () => {
     const config = useRuntimeConfig()
 
     try {
-        await mongoose.connect(
-            config.public.mongodbUrl ??
-                'mongodb+srv://nghiemmanhcuong98hn:0987954221@trello.xk40ihq.mongodb.net/?retryWrites=true&w=majority&appName=trello'
-        )
+        await mongoose.connect('mongodb+srv://nghiemmanhcuong98hn:0987954221@trello.xk40ihq.mongodb.net/?retryWrites=true&w=majority&appName=trello')
         console.log('Connect database successfully!')
     } catch (error) {
         console.log(error)
