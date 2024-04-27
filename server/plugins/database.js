253cd0ee2e75ae443f8d1839a8dfc5974f7f3afb
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2024-04-27 07:45:53 +0700

    unchnage mongose db url

--- server/plugins/database.js
+++ server/plugins/database.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
-export default defineNitroPlugin(async () => {
-    const config = useRuntimeConfig()
+export default defineNitroPlugin(async() => {
+      const config = useRuntimeConfig()
 
-    try {
-        await mongoose.connect('mongodb+srv://nghiemmanhcuong98hn:0987954221@trello.xk40ihq.mongodb.net/?retryWrites=true&w=majority&appName=trello')
-        console.log('Connect database successfully!')
-    } catch (error) {
-        console.log(error)
-    }
+     try {
+            await  mongoose.connect(config.public.mongodbUrl)
+            console.log('Connect database successfully!');
+     } catch (error) {
+            console.log(error);
+     }
 })
