1b86b2e05f51927c39da56c06f1e5cdc55e5966c
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2024-04-28 20:19:24 +0700

    fix bug plugins quill

--- plugins/Quill.client.js
+++ plugins/Quill.client.js
@@ -2,5 +2,5 @@ import { QuillEditor } from '@vueup/vue-quill';
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 export default defineNuxtPlugin(nuxtApp => {
-	// nuxtApp.vueApp.component(QuillEditor);
+	nuxtApp.vueApp.component('QuillEditor',QuillEditor);
 });
