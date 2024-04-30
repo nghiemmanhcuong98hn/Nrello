d60dd7e9612eb1180d3a75920442e76c2f339415
nghiemmanhcuong <nghiemmanhcuong98@gmail.com>
2024-04-30 20:47:01 +0700

    add filed startDate andDate and isDone card model

--- server/schemas/card.schema.js
+++ server/schemas/card.schema.js
@@ -6,8 +6,13 @@ export default z.object({
 			required_error: 'Title is required'
 		})
 		.max(255),
-	description: z.string({
-		required_error: 'Description is required'
+	description: z.string().optional().nullable(),
+	isDone: z.boolean().optional(),
+	startDate: z.string({
+		required_error: 'Start date is required'
+	}),
+	endDate: z.string({
+		required_error: 'End date is required'
 	}),
 	list: z.string().optional()
 });
