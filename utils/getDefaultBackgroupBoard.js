8410facd1387052c7c3f2a15d32c7ea31378072c
nghiemmanhcuong <nghiemmanhcuong198@gmail.com>
2024-05-01 08:33:38 +0700

    add default backgroup board

---
+++ utils/getDefaultBackgroupBoard.js
@@ -0,0 +1,10 @@
+export default function (bgIndex) {
+	const bgList = [
+		'rgb(239 68 68)',
+		'rgb(249 115 22)',
+		'rgb(132 204 22)',
+		'rgb(14 165 233)',
+		'rgb(139 92 246)'
+	];
+	return bgList[bgIndex];
+}
