a26a5578d1ffb9c0f58d4144bceaa7530319ec62
nghiemmanhcuong <nghiemmanhcuong198@gmail.com>
2024-05-07 09:44:54 +0700

    fix ui card detail

--- server/schemas/card.schema.js
+++ server/schemas/card.schema.js
@@ -8,11 +8,5 @@ export default z.object({
 		.max(255),
 	description: z.string().optional().nullable(),
 	isDone: z.boolean().optional(),
-	startDate: z.string({
-		required_error: 'Start date is required'
-	}),
-	endDate: z.string({
-		required_error: 'End date is required'
-	}),
 	list: z.string().optional()
 });
